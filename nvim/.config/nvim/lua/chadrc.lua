56ff72305e39109942d07c5fa44c192108b6c7d2
xxun <xxun10224@gmail.com>
2026-02-09 17:48:22 +0800

    nvim: change theme

--- nvim/.config/nvim/lua/chadrc.lua
+++ nvim/.config/nvim/lua/chadrc.lua
@@ -6,7 +6,7 @@
 local M = {}
 
 M.base46 = {
-	theme = "decay",
+	theme = "yoru",
 
 	-- hl_override = {
 	-- 	Comment = { italic = true },
@@ -15,10 +15,14 @@ M.base46 = {
 }
 
 -- M.nvdash = { load_on_startup = true }
--- M.ui = {
---       tabufline = {
---          lazyload = false
---      }
---}
+M.ui = {
+  tabufline = {
+    lazyload = false,
+    order = { "treeOffset", "buffers" },
+  },
+  statusline = {
+    theme = "vscode",
+  }
+}
 
 return M
