f29f82c9ba8b53aa6e4fc84e79941f3f863c5e52
xxun <xxun10224@gmail.com>
2026-02-09 17:39:55 +0800

    yazi:
    1. update plugins
    2. fix outdated config

--- yazi/.config/yazi/plugins/folder-rules.yazi/main.lua
+++ yazi/.config/yazi/plugins/folder-rules.yazi/main.lua
@@ -17,9 +17,9 @@ return {
         ps.sub("cd", function()
             local dir = tostring(cx.active.current.cwd)
             if string.find(dir, "Downloads") then
-                ya.manager_emit("sort", { "mtime", reverse = true, dir_first = false })
+                ya.mgr_emit("sort", { "mtime", reverse = true, dir_first = false })
             -- else
-            --     ya.manager_emit("sort", { "natural", reverse = false, dir_first = true })
+            --     ya.mgr_emit("sort", { "natural", reverse = false, dir_first = true })
             end
         end)
     end,
--- yazi/.config/yazi/plugins/smart-escape.yazi/main.lua
+++ yazi/.config/yazi/plugins/smart-escape.yazi/main.lua
@@ -5,6 +5,6 @@ return {
     local current = active.current
 
     local esc = active.mode.is_visual or #active.selected > 0 or current.files.filter or current.cwd.is_search
-    ya.manager_emit(esc and "escape" or "close", {})
+    ya.mgr_emit(esc and "escape" or "close", {})
   end,
 }
