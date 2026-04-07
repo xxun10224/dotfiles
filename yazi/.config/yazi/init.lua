7e62813d907087315b92df805d2bdb7db9773753
xxun <xxun10224@gmail.com>
2026-04-07 13:33:49 +0800

    yazi:
    1. fix outdated config for git.yazi apis
    2. update plugins

--- yazi/.config/yazi/init.lua
+++ yazi/.config/yazi/init.lua
@@ -1,6 +1,9 @@
 require("full-border"):setup()
 require("folder-rules"):setup()
-require("git"):setup()
+require("git"):setup {
+	-- Order of status signs showing in the linemode
+	order = 1500,
+}
 
 function Tabs.height()
 	return 0
